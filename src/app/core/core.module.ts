24556e8f755ac029a5be264a5f5f016ec37d5f52
rajsadho <rajsadho@users.noreply.github.com>
2021-12-28 21:55:09 -0400

    Added exports in core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import {NgModule, Optional, SkipSelf} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MovieApiService } from './services';
+
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
   declarations: [],
-  providers: []
+  providers: [MovieApiService]
 })
 
 export class CoreModule {
