252ecb8b4ad10c6acfdd8006ed6c67a802a1794e
rajsadho <rajsadho@users.noreply.github.com>
2021-12-29 03:38:43 -0400

    Slight additions to routing

--- src/app/movies/movie-details/movie-details.component.ts
+++ src/app/movies/movie-details/movie-details.component.ts
@@ -1,7 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { map, throttleTime, filter, switchMap } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { Movie, MovieApiService } from 'src/app/core';
 
 @Component({
     selector: 'movie-details',
     templateUrl: './movie-details.component.html'
 })
-export class MovieDetailsComponent { };
+export class MovieDetailsComponent {
+    constructor(
+        private route: ActivatedRoute, 
+        public restApi: MovieApiService
+    ) {}
+
+    movie: Movie | undefined;
+
+    ngOnInit() {}
+  
+    get movieID() {
+      return this.route.params.pipe(map(({ id }) => 
+        this.loadMovie(+id),
+        console.log(this.movie)
+      ));
+    }
+
+    loadMovie(id: number) {
+        return this.restApi.getMovie(id).subscribe((data: {}) => {
+          console.log(typeof(data))
+          console.log(data);
+          this.movie = data as Movie;
+          console.log(this.movie);
+        })
+      }
+};
